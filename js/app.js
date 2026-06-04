5855acad5a240dd08cd612391e51c856051fa699
prasadrinku77-afk <prasadrinku77@gmail.com>
2026-06-04 17:52:32 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log("MK Institute Website Loaded");
+
+document
+.getElementById("admissionForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Admission form submitted successfully!");
+
+});
