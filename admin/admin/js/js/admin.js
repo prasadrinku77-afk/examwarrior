8b078f162471a2d5e86826ddcbbc1c2cc1719e35
prasadrinku77-afk <prasadrinku77@gmail.com>
2026-06-05 16:34:10 +0530

    Create admin.js

---
+++ admin/admin/js/js/admin.js
@@ -0,0 +1,19 @@
+// =====================================
+// MANISH KUMAR SCHOOL
+// ADMIN PANEL
+// =====================================
+
+console.log("Admin Panel Loaded");
+
+// Future Features
+// 1. Notice Management
+// 2. Course Management
+// 3. Contact Management
+// 4. Admission Management
+// 5. Student Database
+// 6. Image Upload
+// 7. Dashboard Analytics
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Admin Dashboard Ready");
+});
